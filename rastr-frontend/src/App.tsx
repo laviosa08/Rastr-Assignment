9ad31a37cedd89c3f27724beb3276dfd6f6280e0
Saloni Agarwal <saloni@chingari.io>
2024-02-23 17:31:20 +0530

    added frontend

---
+++ rastr-frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Container from "@mui/material/Container";
+import Product from "./pages/Product";
+
+const App: React.FC = () => {
+  return (
+    <Container>
+      <Product />
+    </Container>
+  );
+};
+
+export default App;
